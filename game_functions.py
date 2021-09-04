3f0c85cc911a861a45e8fec7f16a35427afc43dc
matserg <61161987+matserg@users.noreply.github.com>
2021-09-05 01:36:37 +0300

    added the ability to move to the right

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,17 @@ import sys
 import pygame
 
 
-def check_events():
+def check_events(ship):
     """Обрабатывает нажатия клавиш и события мыши"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
 
 
 def update_screen(ai_settings, screen, ship):
